02908e86f66e9754eeef71b644ac15da6bf40a97
Leonardo Batista <104659313+LeoBat00@users.noreply.github.com>
2026-03-18 13:27:58 -0300

    adicionando base para bannerinfo

---
+++ src/componentes/bannerinfo/BannerInfo.jsx
@@ -0,0 +1,11 @@
+import "./BannerInfo.css";
+
+function BannerInfo() {
+  return (
+    <div className="banner-info">
+      <span className="banner-info__titulo">Alterar cadastro</span>
+    </div>
+  );
+}
+
+export default BannerInfo;
